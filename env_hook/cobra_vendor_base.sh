c29368e9cfb554176b553f2b9eb708ef023dd850
Michael Jeronimo <michael.jeronimo@openrobotics.org>
2021-10-29 14:57:22 -0700

    Change cobra configuration method
    Install the cobra rules files into the package share directory.
    Then, instead of invoking "cobra -configure", use and environment
    hook to set the C_BASE environment variable to point to the
    rules directory.
    
    Signed-off-by: Michael Jeronimo <michael.jeronimo@openrobotics.org>

---
+++ env_hook/cobra_vendor_base.sh
@@ -0,0 +1,5 @@
+# copied from cobra_vendor/env_hook/cobra_vendor_base.sh
+
+#ament_prepend_unique_value LD_LIBRARY_PATH "$AMENT_CURRENT_PREFIX"
+
+#ament_prepend_unique_value LD_LIBRARY_PATH "ThisIsTheValue"
